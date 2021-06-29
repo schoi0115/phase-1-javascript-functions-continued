a3c253a97170586552e4bb8b8a3c52ca744b24a3
Shawn <schoi0115@gmail.com>
2021-06-29 01:17:10 +0000

    commit tree f9a501660f78c8c745d9e0ab67549710bb9025a3

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function saturdayFun(layer="roller-skate") {
+  return `This Saturday, I want to ${layer}!`
+}
+saturdayFun();
+    console.log ("This Saturday, I want to bathe my dog!")
+
+let mondayWork = function (office="go to the office") {
+  return `This Monday, I will ${office}.`
+}
+mondayWork();
+    console.log ("This Monday, I will work from home")
+
+let wrapAdjective = function(sign="*") {
+  return function(programmer="a dedicated programmer") {
+    return `You are ${sign}${programmer}${sign}!`
+  }
+}
